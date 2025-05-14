f873c1b75db539a2ec891425465bbacf3f447b5c
dillimaharjan <dilli.maharjan@gmail.com>
2025-05-14 18:35:43 +0545

    Added file copiilot.cmd

---
+++ copilot.cmd
@@ -0,0 +1,23 @@
+@workspace /explain
+create a class diagram using mermaid js for the #pythan_exec_proc.sql
+
+
+
+
+classDiagram
+    class pythian_exec_proc {
+        +varchar(64) @sp_name
+        +int @v_debug = 0
+        +int @proc_exec_status
+        +varchar(200) @msg
+        +executeProcedure()
+        +printDebugMessages()
+        +handleError()
+    }
+
+    class TargetProcedure {
+        <<dynamic>>
+        +int return_code
+    }
+
+    pythian_exec_proc "1" --> "1" TargetProcedure : executes dynamically
